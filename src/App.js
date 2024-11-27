ffa4b24b93e460b730163499e11f64220fc17591
John Doe <sdfinkod@code.edu.az>
2024-11-27 09:44:00 +0400

    progress

--- src/App.js
+++ src/App.js
@@ -1,54 +1,75 @@
-import React, { useEffect, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { decrement, increment, incrementByAmount } from './redux/reducers/counterSlice'
-import { getProductThunk } from './redux/reducers/productsSlice'
-import { postFormThunk } from './redux/reducers/formSlice'
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  decrement,
+  increment,
+  incrementByAmount,
+} from "./redux/reducers/counterSlice";
+import {
+  deleteProductThunk,
+  getProductsThunk,
+  postProductThunk,
+} from "./redux/reducers/productsSlice";
+import { postFormThunk } from "./redux/reducers/formSlice";
 
 const App = () => {
+  const [name, setName] = useState("");
+  const [des, setDes] = useState("");
 
-  const [name, setName] = useState('')
+  const count = useSelector((state) => state.counter.value);
 
-  const count = useSelector((state) => state.counter.value)
+  const products = useSelector((state) => state.products.products);
+  const loading = useSelector((state) => state.products.loading);
+  const error = useSelector((state) => state.products.error);
 
-  const products = useSelector((state) => state.products.products)
-  const loading = useSelector((state) => state.products.loading)
-  const error = useSelector((state) => state.products.error)
-
-  const formError = useSelector((state) => state.form.error)
-  
-
-  const dispacth = useDispatch()
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    dispacth(getProductThunk())
-  }, [])
+    dispatch(getProductsThunk());
+  }, []);
 
+  if (loading) return <p>LLOOAADDIINNGG</p>;
+  if (error) return <p>Xeta bas verdi</p>;
 
-  const sendName = () => {
-    dispacth(postFormThunk({name:name}))
+  const sendProduct = () => {
+    dispatch(postProductThunk({ name: name, description: des }));
+  };
+
+  const deleteProduct = (id) => {
+    dispatch(deleteProductThunk(id))
   }
-
-  if (loading) return <p>LLOOOAAADDDIIINNNNGGG</p>
-  if (error) return <p>Xeta bas verdi</p>
-  if (formError) return <p>Gondererken Xeta bas verdi</p>
 
   return (
     <div>
-      <input type='text' placeholder='name' value={name} onChange={(e) => setName(e.target.value)}/>
-      <button onClick={() => sendName()}>SEND YOUR NAME</button>
+      <input
+        placeholder="name"
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <input
+        placeholder="name"
+        type="text"
+        value={des}
+        onChange={(e) => setDes(e.target.value)}
+      />
+      <button onClick={() => sendProduct()}>SEND YOUR NAME</button>
+
+      {products &&
+        products.map((item) => {
+          return (
+            <div>
+              <p>{item.name}</p> <button onClick={() => deleteProduct(item.id)}>Delete</button>
+            </div>
+          );
+        })}
 
       {count}
-      <button onClick={() => dispacth(increment())}>+++</button>
-      <button onClick={() => dispacth(decrement())}>---</button>
-      <button onClick={() => dispacth(incrementByAmount(20))}>+20</button>
-
-
-      {products && products.map( item => {
-        return <p>{item.title}</p>
-      })}
-
+      <button onClick={() => dispatch(increment())}>+++</button>
+      <button onClick={() => dispatch(decrement())}>---</button>
+      <button onClick={() => dispatch(incrementByAmount(20))}>+20</button>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/redux/reducers/productsSlice.js
+++ src/redux/reducers/productsSlice.js
@@ -1,30 +1,47 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
-export const getProductThunk = createAsyncThunk('api/products', async () => {
-    const response = await axios.get('https://dummyjson.com/products')    
-    return response.data.products
+
+export const getProductsThunk = createAsyncThunk( 'api/products', async () => {
+    const response = await axios.get('https://northwind.vercel.app/api/categories')
+    return response.data
+})
+
+export const postProductThunk = createAsyncThunk('api/post', async (data) => {
+    const response = await axios.post('https://northwind.vercel.app/api/categories', data)
+    return response.data
+}) 
+
+export const deleteProductThunk = createAsyncThunk('product/del', async (id) => {
+    const response = await axios.delete(`https://northwind.vercel.app/api/categories/${id}`)
+    return id
 })
 
 
 export const productsSlice = createSlice({
     name:'products',
-    initialState: {},
-    reducers:{
-         
-    },
-    extraReducers: (builder) => {
-        builder
-        .addCase(getProductThunk.fulfilled, (state, action) => {
+    initialState:{},
+    extraReducers: builder => {
+        builder 
+        //GET
+        .addCase(getProductsThunk.fulfilled, (state, action) => {
             state.loading = false
             state.products = action.payload
         })
-        .addCase(getProductThunk.pending, (state) => {
+        .addCase(getProductsThunk.pending, (state) => {
             state.loading = true
         })
-        .addCase(getProductThunk.rejected, (state, action) => {
+        .addCase(getProductsThunk.rejected, (state, action) => {
             state.loading = false
             state.error = action.error
+        })
+        //POST
+        .addCase(postProductThunk.fulfilled, (state, action) => {
+            state.products.push(action.payload)
+        })
+        //DELETE
+        .addCase(deleteProductThunk.fulfilled, (state, action) => {
+            state.products = state.products.filter(item => item.id != action.payload)
         })
     }
 })
